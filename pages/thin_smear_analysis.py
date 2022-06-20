ffafdd81f45d508db5832cbe096a475523d5ae21
anissacherar <aniss.acherar@gmail.com>
2022-06-20 15:07:43 +0200

    update model dir

--- pages/thin_smear_analysis.py
+++ pages/thin_smear_analysis.py
@@ -35,7 +35,7 @@ from typing import List, NamedTuple
 
 
 #load the model 
-local=" /app/deepcare/model/model_5_ResNet.h5"
+local="model/model_5_ResNet.h5"
 #reference=np.asarray(Image.open("2846_IMG_9391_431.jpg"))
 dim = (50, 50)
 
