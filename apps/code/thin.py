a6caf24e9b3e9fbd3f3dda8c66eef9c1efe32022
anissacherar <aniss.acherar@gmail.com>
2022-06-21 11:21:03 +0200

    update delete main

--- apps/code/thin.py
+++ apps/code/thin.py
@@ -316,7 +316,7 @@ def cropChamp(image):
     # cv2.imwrite("resultats/evaluation_images/odm/" + (str(image).split('/')[-1]).split('.')[0] + ".jpg", champ)
     return champ
 
-#@st.cache(allow_output_mutation=True, max_entries=10, ttl=3600) #online uniquement
+@st.cache(allow_output_mutation=True, max_entries=10, ttl=3600) #online uniquement
 def exam(champ,model):
     # champ2 = cv2.fastNlMeansDenoisingColored(champ, None, 10, 10, 21, 7)
     plt.figure(figsize=(10, 10))
