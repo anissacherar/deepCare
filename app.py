c92e1d3c03a46f1c39b82cd6c40081f7ebfb8912
anissacherar <aniss.acherar@gmail.com>
2022-06-21 11:03:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -26,6 +26,13 @@ st.session_state["warned_about_save_answers"] = True
 
 
 
+col1, col2 = st.columns(2)
+
+# Title of the main page
+display = Image.open('logo.JPG')
+display = np.array(display)
+st.image(display, width = 120)
+st.title("DeepCare")
 
 current_path = Path(__file__).parent.absolute()
 stb.set_book_config(menu_title="Main Menu",
@@ -52,24 +59,17 @@ stb.set_book_config(menu_title="Main Menu",
 
 # Create an instance of the app 
 #app = MultiPage()
-col1, col2 = st.columns(2)
-
-# Title of the main page
-display = Image.open('logo.JPG')
-display = np.array(display)
-st.image(display, width = 120)
-st.title("DeepCare")
 
 #col1.image(display, width = 120)
 
 #col2.title("")
 
 # Add all your application here
-"""app.add_page("THIN SMEAR", thin_smear_analysis.app)
-app.add_page("THICK SMEAR", thick_smear_analysis.app)
+#app.add_page("THIN SMEAR", thin_smear_analysis.app)
+#app.add_page("THICK SMEAR", thick_smear_analysis.app)
 
 # The main app
-app.run()"""
+#app.run()"""
 
 
 
