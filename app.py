ba6e24d4ec85cd65fd66e35f427e4a5ea58a8f4c
anissacherar <aniss.acherar@gmail.com>
2022-06-21 14:10:58 +0200

    update logo png

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ st.set_page_config(
 
 
 # Title of the main page
-display = Image.open('logo_shadow.jpg')
+display = Image.open('logo.png')
 display = np.array(display)
 st.image(display, width = 120)
 st.title("DeepCare")
