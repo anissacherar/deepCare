bba0b93d2dcbaa1383fa434924932f563081508d
anissacherar <aniss.acherar@gmail.com>
2022-06-21 12:41:14 +0200

    Update thin.py

--- apps/code/thin.py
+++ apps/code/thin.py
@@ -316,7 +316,8 @@ def cropChamp(image):
     # cv2.imwrite("resultats/evaluation_images/odm/" + (str(image).split('/')[-1]).split('.')[0] + ".jpg", champ)
     return champ
 
-@st.cache(allow_output_mutation=True, max_entries=10, ttl=3600) #online uniquement
+#@st.cache(allow_output_mutation=True, max_entries=10, ttl=3600) #online uniquement
+@st.cache(show_spinner=False)
 def exam(champ,model):
     # champ2 = cv2.fastNlMeansDenoisingColored(champ, None, 10, 10, 21, 7)
     plt.figure(figsize=(10, 10))
@@ -420,6 +421,7 @@ def exam(champ,model):
 
     return champ_seg, grp, p
 
+@st.cache(show_spinner=False)
 def exam_x1000(champ,model):
     # champ2 = cv2.fastNlMeansDenoisingColored(champ, None, 10, 10, 21, 7)
     champ_seg = champ.copy()  # nouveau champ pour dessiner les bbox et le périmêtre d'analyse
