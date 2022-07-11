8a826f8fc7c4af56cfe3fb8f00f8b0c894dc1f4b
anissacherar <aniss.acherar@gmail.com>
2022-07-11 13:30:51 +0200

    Update contact.py

--- apps/contact.py
+++ apps/contact.py
@@ -5,13 +5,8 @@ with st.form("my_form", clear_on_submit=True):
     name=st.text_input("Enter full name")
     email=st.text_input("Enter email")
     message=st.text_area("Message")
-    st.write("Inside the form")
-    slider_val = st.slider("Form slider")
-    checkbox_val = st.checkbox("Form checkbox")
 
     # Every form must have a submit button.
     submitted = st.form_submit_button("Submit")
     if submitted:
-        st.write("slider", slider_val, "checkbox", checkbox_val)
-
-st.write("Thanks for your message !")
+        st.write("Thanks for your message !")
